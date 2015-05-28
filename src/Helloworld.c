90a5134a8fe16dfb8ba8e371e50dc5193d0ef5fe
Liu Chen <July_carbon@hotmail.com>
2015-05-28 09:26:35 +0800

    Wrote Helloworld.c file

---
+++ src/Helloworld.c
@@ -0,0 +1,35 @@
+/*
+ ============================================================================
+ Name        : Helloworld.c
+ Author      : Walter
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <math.h>
+
+#define PROCESS_SUCCESS 0
+
+int main(void) {
+	double theta = 0.5;
+
+	double d_cos = cos(theta);
+	double d_sin = sin(M_PI);
+	double d_sqrt = sqrt(7);
+	double d_log = log(10);
+	double d_exp = exp(2);
+
+	printf("cal:cos(theta) = %lf\n",d_cos);
+	printf("cal:sin(pi) = %lf\n",d_sin);
+	printf("cal:sqrt(7) = %lf\n",d_sqrt);
+	printf("cal:log(10) = %lf\n",d_log);
+	printf("cal:exp(2) = %lf\n",d_exp);
+
+	puts("Hello HiLinux!!!"); /* prints Hello World!!! */
+	return PROCESS_SUCCESS;
+}
