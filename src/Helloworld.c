7490306b0553c2e12f7a5ff71c2d4883eb6733ce
WalterYoo <july_carbon@hotmail.com>
2015-07-22 23:40:00 +0800

    Modify Helloworld.c

--- src/Helloworld.c
+++ src/Helloworld.c
@@ -16,23 +16,23 @@
 #define PROCESS_SUCCESS 0
 
 int main(void) {
-	double theta = 0.5;
+	//double theta = 0.5;
 
-	double d_cos = cos(theta);
-	double d_sin = sin(M_PI);
-	double d_sqrt = sqrt(7);
-	double d_log = log(10);
-	double d_exp = exp(2);
-	double d_log2 = log(2);
-	double d_pow2_5 = pow(2,5);
+	//double d_cos = cos(theta);
+	//double d_sin = sin(M_PI);
+	//double d_sqrt = sqrt(7);
+	//double d_log = log(10);
+	//double d_exp = exp(2);
+	//double d_log2 = log(2);
+	//double d_pow2_5 = pow(2,5);
 
-	printf("cal:cos(theta) = %lf\n",d_cos);
-	printf("cal:sin(pi) = %lf\n",d_sin);
-	printf("cal:sqrt(7) = %lf\n",d_sqrt);
-	printf("cal:log(10) = %lf\n",d_log);
-	printf("cal:exp(2) = %lf\n",d_exp);
-	printf("cal:log(2) = %lf\n",d_log2);
-	printf("cal:pow(2,5) = %lf\n",d_pow2_5);
+	//printf("cal:cos(theta) = %lf\n",d_cos);
+	//printf("cal:sin(pi) = %lf\n",d_sin);
+	//printf("cal:sqrt(7) = %lf\n",d_sqrt);
+	//printf("cal:log(10) = %lf\n",d_log);
+	//printf("cal:exp(2) = %lf\n",d_exp);
+	//printf("cal:log(2) = %lf\n",d_log2);
+	//printf("cal:pow(2,5) = %lf\n",d_pow2_5);
 
 	puts("Hello HiLinux!!!"); /* prints Hello World!!! */
 	return PROCESS_SUCCESS;
